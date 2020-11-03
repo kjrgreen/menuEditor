8b8357306859917675cc1d8055ca66183c9b4212
kjrgreen <kevin.j.r.green@gmail.com>
2020-11-03 23:44:45 +0100

    Add placeholder menu editing page This page requires authentication to reach, but can be reached by any authenticated user. Link to it placed in index.html

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using menuEditor.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authorization;
 
 namespace menuEditor
 {
@@ -33,6 +34,22 @@ namespace menuEditor
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            services.AddControllers();
+
+            services.AddRazorPages(options =>
+                {
+                    options.Conventions.AllowAnonymousToPage("/Index");
+                    options.Conventions.AllowAnonymousToPage("/Privacy");
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
