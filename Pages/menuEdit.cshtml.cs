98e791b6deba5f06236de961b7a5b24116921fe3
kjrgreen <kevin.j.r.green@gmail.com>
2020-11-04 01:02:57 +0100

    Add support for adding top-level menu items

--- Pages/menuEdit.cshtml.cs
+++ Pages/menuEdit.cshtml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using menuEditor.Data;
+using menuEditor.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace menuEditor.Pages
@@ -11,15 +14,18 @@ namespace menuEditor.Pages
     public class menuEditModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public menuEditModel(ILogger<IndexModel> logger)
+        public List<MenuItem> MenuItems { get; set; }
+        public menuEditModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            MenuItems = await _context.menuItems.ToListAsync();
         }
     }
 }
---
+++ Pages/menuItemScaffolded.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using menuEditor.Data;
+using menuEditor.Data.Models;
+
+namespace menuEditor.Pages
+{
+    public class menuItemScaffoldedModel : PageModel
+    {
+        private readonly menuEditor.Data.ApplicationDbContext _context;
+
+        public menuItemScaffoldedModel(menuEditor.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public MenuItem MenuItem { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.menuItems.Add(MenuItem);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./menuEdit");
+        }
+    }
+}
